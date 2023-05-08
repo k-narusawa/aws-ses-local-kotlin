039cfcafa7005bb3746220b3f5b800807bb7b7bf
k-narusawa <narusawakohei@team-lab.com>
2023-05-08 10:37:42 +0900

    style: 💄 検索機能の追加

--- src/main/kotlin/com/knarusawa/mock/ses/sesmock/controller/SESController.kt
+++ src/main/kotlin/com/knarusawa/mock/ses/sesmock/controller/SESController.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/")
@@ -12,8 +13,11 @@ class SESController(
     private val mailService: MailService
 ) {
     @GetMapping
-    fun index(model: Model): String {
-        val entities = mailService.getEmails(page = 0, size=10, to = null)
+    fun index(
+        model: Model,
+        @RequestParam(name = "to") to: String? = null,
+    ): String {
+        val entities = mailService.getEmails(page = 0, size=10, to = to)
         model.addAttribute("mails", entities.emails)
         return "index"
     }
